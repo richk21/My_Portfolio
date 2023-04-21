31d66c96bfc17a2df6152e049fe7703bb427b0d5
richk21 <64418209+richk21@users.noreply.github.com>
2023-04-21 12:40:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/My_Portfolio/",
   plugins: [react()],
   commonjsOptions: {
     esmExternals: true,
