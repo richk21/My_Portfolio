480bd5dee6369748303611286587ba5195e3242a
richa <richa21kiran@gmail.com>
2025-07-13 16:42:39 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
   plugins: [react()],
   commonjsOptions: {
     esmExternals: true,
- },
+  },
 });
