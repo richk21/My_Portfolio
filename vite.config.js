fd01a1a571752f420dfd1a3afb1a3117f65d7850
richa <richa21kiran@gmail.com>
2025-07-13 17:28:54 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      external: ["three"],
+    },
   },
   plugins: [react()],
   commonjsOptions: {
