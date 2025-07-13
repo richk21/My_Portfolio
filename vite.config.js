9d799071874167a3a97370632e14192ba2dc10df
richa <richa21kiran@gmail.com>
2025-07-13 19:24:47 +0530

    dist updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: ".",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
       external: ["three"],
