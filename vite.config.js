f1992b075b889de817072371934494830aa2057a
richa <richa21kiran@gmail.com>
2025-07-13 16:50:15 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./src",
+  root: ".",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
